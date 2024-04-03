a778dab229d1b568f22fbc912828f530c8492d2a
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-04-03 21:08:19 +0000

    Update adonisbypass.lua

--- BetterBypasser/resources/adonisbypass.lua
+++ BetterBypasser/resources/adonisbypass.lua
@@ -1,57 +1,37 @@
 -- https://v3rm.net/threads/trading-adonis-bypass-i-made-for-krampus-key.4730/post-33749
-
-local getinfo = getinfo or debug.getinfo
-local DEBUG = false
 local Hooked = {}
 
-local Detected, Kill
-
-setthreadidentity(2)
-
-for i, v in getgc(true) do
-    if typeof(v) == "table" then
-        local DetectFunc = rawget(v, "Detected")
-        local KillFunc = rawget(v, "Kill")
-    
-        if typeof(DetectFunc) == "function" and not Detected then
-            Detected = DetectFunc
+local function handleAdonis()
+    for _, obj in ipairs(getgc(true)) do
+        if type(obj) == "table" then
+            local DetectedFunc = rawget(obj, "Detected")
+            local KillFunc = rawget(obj, "Kill")
             
-            local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
-                if Action ~= "_" then
-                    if DEBUG then
-                        warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
-                    end
-                end
-                
-                return true
-            end)
-
-            table.insert(Hooked, Detected)
-        end
-
-        if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
-            Kill = KillFunc
-            local Old; Old = hookfunction(Kill, function(Info)
-                if DEBUG then
-                    warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
-                end
-            end)
-
-            table.insert(Hooked, Kill)
+            if type(DetectedFunc) == "function" then
+                Detected = DetectedFunc
+                hookfunction(Detected, function(Action, Info, NoCrash)
+                    return true
+                end)
+                table.insert(Hooked, Detected)
+            end
+            
+            if type(obj.Variables) == "table" and type(obj.Process) == "function" and type(KillFunc) == "function" then
+                Kill = KillFunc
+                hookfunction(Kill, function(Info)
+                end)
+                table.insert(Hooked, Kill)
+            end
         end
     end
 end
 
-local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+local oldInfo = hookfunction(getrenv().debug.info, newcclosure(function(...)
     local LevelOrFunc, Info = ...
-
     if Detected and LevelOrFunc == Detected then
-        if DEBUG then
-            warn(`Adonis AntiCheat sanity check detected and broken`)
-        end
-
         return coroutine.yield(coroutine.running())
     end
-    
-    return Old(...)
+    return oldInfo(...)
 end))
+
+handleAdonis()
+
