f388086c40f143078f21dd9c2269a0adb1c1ef13
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-06-29 21:04:49 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -1,19 +1,18 @@
-local executorEnviroment = debug.getfenv()
-local Services = setmetatable({}, {
-	__index = function(_, serviceName)
-		if debug.getfenv(2) ~= executorEnviroment then
-			return nil;
-		end
-		return cloneref(game[serviceName])
-	end
-})
-
-local player = Services.Players.LocalPlayer
-repeat task.wait() until game:IsLoaded() and player.Character
-
-getgenv().options = ({...})[1]
-
 local success, error = pcall(function()
+	local executorEnviroment = debug.getfenv()
+	local Services = setmetatable({}, {
+		__index = function(_, serviceName)
+			if debug.getfenv(2) ~= executorEnviroment then
+				return nil;
+			end
+			return cloneref(game[serviceName])
+		end
+	})
+	
+	local player = Services.Players.LocalPlayer
+	repeat task.wait() until game:IsLoaded() and player.Character
+	
+	getgenv().options = ({...})[1]
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua",true))()
 end)
 
