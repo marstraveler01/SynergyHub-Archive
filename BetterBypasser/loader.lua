db4ab2540b6847b3440b8704c78b7e2b50e1c57f
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-08-15 17:14:00 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -20,4 +20,5 @@ local player = Services.Players.LocalPlayer
 repeat task.wait() until game:IsLoaded() and player.Character
 
 getgenv().OptionsSettings = ({...})[1]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua",true))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua",true))()
+player:Kick("BetterBypasser patched until further notice.")
