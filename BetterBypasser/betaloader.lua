146f6a49f1763b015c3479a5e6049121b74d0d18
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-08-17 18:13:56 +0000

    Update betaloader.lua

--- BetterBypasser/betaloader.lua
+++ BetterBypasser/betaloader.lua
@@ -11,5 +11,4 @@ local Services = setmetatable({}, {
 local player = Services.Players.LocalPlayer
 repeat task.wait() until game:IsLoaded() and player.Character
 
-getgenv().OptionsSettings = ({...})[1]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/alphaproduct.lua",true))()
