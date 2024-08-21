768a753c73c8c3128e64e1e8d5d15ccc96e9f20a
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-08-21 21:20:22 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -4,21 +4,12 @@ end
 
 local Services = setmetatable({}, {
 	__index = function(_, serviceName)
-		local success, service = pcall(function()
-			return game:GetService(serviceName)
-		end)
-		if success then
-			return cloneref(service)
-		else
-			warn("Service not found: " .. serviceName)
-			return nil
-		end
+		return game:GetService(serviceName)
 	end
 })
 
 local player = Services.Players.LocalPlayer
 repeat task.wait() until game:IsLoaded() and player.Character
 
-getgenv().OptionsSettings = ({...})[1]
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua",true))()
 player:Kick("BetterBypasser patched until further notice. Join our Discord via vaultcord.win/synergy")
