2a055b16dc990bd462bcf1e165177cc8b91cf8df
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-09-26 18:04:08 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -10,4 +10,4 @@ repeat task.wait() until game:IsLoaded() and player.Character
 if getgenv().OptionsSettings ~= nil then player:Kick("This version is outdated. Join vaultcord.win/synergy, then to the scripts channel to get the script") end;
 player:Kick("This version is patched. Join vaultcord.win/synergy to get status updates.")
 
-..loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua", true))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua", true))()
