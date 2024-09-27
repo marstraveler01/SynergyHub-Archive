7687a82979a790293e7bce707ff98a09e5e22cbf
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-09-27 20:09:42 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -8,6 +8,5 @@ local player = Services.Players.LocalPlayer
 repeat task.wait() until game:IsLoaded() and player.Character
 
 if getgenv().OptionsSettings ~= nil then player:Kick("This version is outdated. Join vaultcord.win/synergy, then to the scripts channel to get the script") end;
-player:Kick("This version is patched. Join vaultcord.win/synergy to get status updates.")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua", true))()
