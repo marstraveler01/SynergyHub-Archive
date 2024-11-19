4e05425645853d825ddac6e465d591948bb7c398
synnyyy <121168410+synnyyy@users.noreply.github.com>
2024-11-19 21:44:31 +0000

    Update loader.lua

--- BetterBypasser/loader.lua
+++ BetterBypasser/loader.lua
@@ -5,10 +5,22 @@ local Services = setmetatable({}, {
 })
 
 local Players = Services.Players
+
 local Player = Players.LocalPlayer
+local IsUnder13 = Player:GetUnder13()
 
 if getgenv().OptionsSettings ~= nil then 
-    Player:Kick("This version is outdated. Join vaultcord.win/synergy, then to the scripts channel to get the script") 
+    return Player.Kick(Player, "This version is outdated. Join vaultcord.win/synergy, then to the scripts channel to get the script") 
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua", true))()
+if IsUnder13 then 
+    return Player.Kick(Player, "You are using a account that is younger than 13. Please use a account that is older than 13 to continue.") 
+end
+
+local Success, Error = pcall(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/publicproduct.lua", true))()
+end)
+
+if not Success and Error then
+	return Player.Kick(Player, "Report this error to vaultcord.win/synergy. Error: " .. Error) 
+end
